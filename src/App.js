9873354f0e098c0847229734dc250806d5eab066
Glen <loboglen05@gmail.com>
2025-08-07 12:59:04 +0530

    front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import WalletApl from './walletapl';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <WalletApl />
+    </div>
+  );
+}
+
+export default App;
